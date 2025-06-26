636ab3ed0a43fbdf9b39cfa126057fd2be301a65
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-06-26 20:21:03 +0600

    Understood action, payload, reducer etc

--- Practice-1/my-app/src/App.jsx
+++ Practice-1/my-app/src/App.jsx
@@ -1,11 +1,32 @@
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from './redux/feature/counterSlice';
 
 function App() {
+  const count = useSelector(state => state.counter.value);
+  const dispatch = useDispatch();
+
+  function handleIncrement() {
+    dispatch(increment());
+  }
+
+  function handleDecrement() {
+    dispatch(decrement());
+  }
+
+  function handleIncrementByAmount() {
+    dispatch(incrementByAmount(5));
+  }
   return (
     <div>
-      <h1>Count : 0</h1>
-      <button>Increment</button>
-      <button>Decrement</button>
+      <h1>Count : {count}</h1>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+      <button onClick={handleIncrementByAmount}>Increment By Amount</button>
     </div>
   );
 }
--- Practice-1/my-app/src/redux/store.js
+++ Practice-1/my-app/src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './feature/counterSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterReducer,
+  },
 });
