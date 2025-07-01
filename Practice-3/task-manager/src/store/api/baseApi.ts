b7b1aa359589917f29d736517903db24238d588c
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-07-01 21:17:17 +0600

    Caching in redux

--- Practice-3/task-manager/src/store/api/baseApi.ts
+++ Practice-3/task-manager/src/store/api/baseApi.ts
@@ -3,9 +3,11 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const baseApi = createApi({
   reducerPath: 'baseApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api' }),
+  tagTypes: ['Task'],
   endpoints: builder => ({
     getTaskAll: builder.query({
       query: () => '/tasks',
+      providesTags: ['Task'],
     }),
 
     createTask: builder.mutation({
@@ -17,6 +19,7 @@ export const baseApi = createApi({
           'Content-Type': 'application/json',
         },
       }),
+      invalidatesTags: ['Task'],
     }),
   }),
 });
