c25b0eb7d12bd8a4ae44931b93d8e2aceb39f61d
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-07-04 20:56:17 +0600

    add vercel.json file

--- B5A4/client/src/store/features/apiSlice.ts
+++ B5A4/client/src/store/features/apiSlice.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:5000/api',
+    baseUrl: '/api',
   }),
   tagTypes: ['Books', 'Borrows'],
   endpoints: builder => ({
